193ded3cca50906800f9bd269cbd5fb5477f773b
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-14 21:37:57 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,46 +5,28 @@ const urlsToCache = [
     '/styles.css',
     '/app.js',
     '/logo.png',
-    '/icon.png',
-    '/icon-large.png',
-    '/manifest.json',
+    '/favicon.ico',
     '/favicon-48x48.png',
     '/favicon.svg',
-    '/favicon.ico',
-    '/apple-touch-icon.png'
+    '/apple-touch-icon.png',
+    '/icon.png',
+    '/icon-large.png',
 ];
 
-// Instalação do Service Worker
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Arquivos em cache');
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-// Ativação do Service Worker
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Interceptação de requisições e busca no cache
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
     );
 });
